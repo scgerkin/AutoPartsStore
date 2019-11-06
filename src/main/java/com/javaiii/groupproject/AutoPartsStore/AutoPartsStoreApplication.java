9a802780341bab2c1fc12ab470c538622643835b
Stephen Gerkin <stephen.gerkin@gmail.com>
2019-11-06 11:03:46 -0500

    Initial commit

---
+++ src/main/java/com/javaiii/groupproject/AutoPartsStore/AutoPartsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.javaiii.groupproject.AutoPartsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPartsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPartsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaiii/groupproject/AutoPartsStore/AutoPartsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaiii.groupproject.AutoPartsStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPartsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
