e712e2d8b23d29d17d51d3cca18177b12de2a7ea
Stephen Gerkin <stephen.gerkin@gmail.com>
2019-11-08 00:46:23 -0500

    Editing an existing employee now works.

--- src/main/java/com/javaiii/groupproject/AutoPartsStore/Controllers/employee/EmployeeController.java
+++ src/main/java/com/javaiii/groupproject/AutoPartsStore/Controllers/employee/EmployeeController.java
@@ -2,12 +2,14 @@ package com.javaiii.groupproject.AutoPartsStore.Controllers.employee;
 
 import com.javaiii.groupproject.AutoPartsStore.DataAccess.DatabaseManager;
 import com.javaiii.groupproject.AutoPartsStore.Models.people.Employee;
+import com.javaiii.groupproject.AutoPartsStore.command.EditEmployeeCommand;
 import com.javaiii.groupproject.AutoPartsStore.command.IdCommand;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -39,10 +41,63 @@ public class EmployeeController {
                                    BindingResult bindingResult,
                                    Model model,
                                    RedirectAttributes redirectAttributes) {
+        Employee editEmployee = null;
+        try {
+            editEmployee = db.getEmployeeByID(id.getId());
+        }
+        catch (SQLException ex) {
+            System.err.println("Error retrieving employee by ID from database");
+            ex.printStackTrace();
+            return "error/databaseError";
+        }
 
-        return "employees/employeeList";
+        EditEmployeeCommand editCmd = new EditEmployeeCommand(editEmployee);
+
+        redirectAttributes.addFlashAttribute("employee", editCmd);
+
+        return "redirect:/employees/editEmployee";
     }
 
+    /**This lets us catch the redirect to edit an employee from the employee list*/
+    @RequestMapping(value="/employees/editEmployee")
+    public String initEditEmployee(@ModelAttribute("employee") EditEmployeeCommand cmd,
+                                   BindingResult bindingResult,
+                                   Model model,
+                                   RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            System.err.println("Binding result has errors");
+        }
+        redirectAttributes.addFlashAttribute("employee", cmd);
+        return "employees/editEmployee";
+    }
+
+    @PostMapping(value="/employees/editEmployee")
+    public String postEditEmployee(@ModelAttribute("employee") EditEmployeeCommand cmd,
+                                   BindingResult bindingResult,
+                                   Model model,
+                                   RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            System.err.println("Binding result has errors");
+        }
+
+        try {
+            Employee employee = cmd.unpack();
+            db.saveToDatabase(employee);
+            buildEmployeeList();
+        }
+        catch (SQLException ex) {
+            System.err.println("Error writing employee to database");
+            ex.printStackTrace();
+            return "errors/databaseError";
+        }
+
+        return "employees/updateSuccess";
+    }
+
+    @RequestMapping(value="/employees/updateSuccess")
+    public String initUpdateSuccessDisplay() {
+        return "employees/updateSuccess";
+    }
 
 
 
---
+++ src/main/java/com/javaiii/groupproject/AutoPartsStore/command/EditEmployeeCommand.java
@@ -0,0 +1,45 @@
+package com.javaiii.groupproject.AutoPartsStore.command;
+
+import com.javaiii.groupproject.AutoPartsStore.Models.address.Address;
+import com.javaiii.groupproject.AutoPartsStore.Models.people.Employee;
+import lombok.Data;
+
+@Data
+public class EditEmployeeCommand {
+    public Integer idNumber;
+    public String lastName;
+    public String firstName;
+    public String title;
+    public String email;
+    public String primaryPhone;
+    public String secondaryPhone;
+    public String street;
+    public String city;
+    public String state;
+    public String zipCode;
+    public String notes;
+
+    public EditEmployeeCommand() {}
+
+    public EditEmployeeCommand(Employee employee) {
+        this.idNumber = employee.getIdNumber();
+        this.lastName = employee.getLastName();
+        this.firstName = employee.getFirstName();
+        this.email = employee.getEmail();
+        this.title = employee.getTitle();
+        this.primaryPhone = employee.getPrimaryPhone();
+        this.secondaryPhone = employee.getSecondaryPhone();
+        this.street = employee.getAddress().getStreet();
+        this.city = employee.getAddress().getCity();
+        this.state = String.valueOf(employee.getAddress().getState());
+        this.zipCode = String.valueOf(employee.getAddress().getZipCode());
+        this.notes = employee.getNotes();
+    }
+
+    public Employee unpack() {
+        Address address = new Address(street, city, state, zipCode);
+
+        return Employee.createExisting(idNumber, lastName, firstName, email,
+            primaryPhone, secondaryPhone, address, notes, title);
+    }
+}
