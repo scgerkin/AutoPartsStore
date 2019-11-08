ba39b5a118551dbc674e019c9d1124688a64c790
Stephen Gerkin <stephen.gerkin@gmail.com>
2019-11-08 11:35:19 -0500

    Modify EditEmployeeCommand to function better with empty values.
    Removed fields as public and created getters/setters.
    Fields that are able to be null are now null if the value given is empty
    instead of being an empty string. This was previously causing a problem
    with creating objects sometimes.

--- src/main/java/com/javaiii/groupproject/AutoPartsStore/command/EditEmployeeCommand.java
+++ src/main/java/com/javaiii/groupproject/AutoPartsStore/command/EditEmployeeCommand.java
@@ -2,22 +2,20 @@ package com.javaiii.groupproject.AutoPartsStore.command;
 
 import com.javaiii.groupproject.AutoPartsStore.Models.address.Address;
 import com.javaiii.groupproject.AutoPartsStore.Models.people.Employee;
-import lombok.Data;
 
-@Data
 public class EditEmployeeCommand {
-    public Integer idNumber;
-    public String lastName;
-    public String firstName;
-    public String title;
-    public String email;
-    public String primaryPhone;
-    public String secondaryPhone;
-    public String street;
-    public String city;
-    public String state;
-    public String zipCode;
-    public String notes;
+    private Integer idNumber;
+    private String lastName;
+    private String firstName;
+    private String title;
+    private String email;
+    private String primaryPhone;
+    private String secondaryPhone;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String notes;
 
     public EditEmployeeCommand() {}
 
@@ -46,9 +44,105 @@ public class EditEmployeeCommand {
     public Employee unpackNew() {
         Address address = new Address(street, city, state, zipCode);
 
-        secondaryPhone = (secondaryPhone.isEmpty()) ? null : secondaryPhone;
+
 
         return Employee.createNew(lastName, firstName, email, primaryPhone,
             secondaryPhone, address, notes, title);
+    }
+
+    public Integer getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(Integer idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title.isEmpty() ? null : title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrimaryPhone() {
+        return primaryPhone;
+    }
+
+    public void setPrimaryPhone(String primaryPhone) {
+        this.primaryPhone = primaryPhone;
+    }
+
+    public String getSecondaryPhone() {
+        return secondaryPhone;
+    }
+
+    public void setSecondaryPhone(String secondaryPhone) {
+        secondaryPhone = secondaryPhone.isEmpty() ? null : secondaryPhone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes.isEmpty() ? null : notes;
     }
 }
