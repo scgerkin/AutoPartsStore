4fc29850487e50c56ebdb5c47a5d6693a43b6612
Stephen Gerkin <stephen.gerkin@gmail.com>
2019-11-11 12:48:42 -0500

    reset db connection to remote

--- src/main/java/com/javaiii/groupproject/AutoPartsStore/AutoPartsStoreApplication.java
+++ src/main/java/com/javaiii/groupproject/AutoPartsStore/AutoPartsStoreApplication.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class AutoPartsStoreApplication {
 
 	public static void main(String[] args) {
-		DatabaseManager db = new DatabaseManager(true);
+		DatabaseManager db = new DatabaseManager(false);
 		SupplierController.setDb(db);
 		EmployeeController.setDb(db);
 		ResupplyOrderController.setDb(db);
