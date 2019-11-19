3cc0f90b7f72af1bf9caa14b3ea0abb913074bdb
Stephen Gerkin <stephen.gerkin@gmail.com>
2019-11-18 20:54:23 -0500

    Remove Lombok annotation and make POJO.
    Not sure how/if Lombok is working right now.

--- src/main/java/com/javaiii/groupproject/AutoPartsStore/services/consumers/commands/SupplierCommand.java
+++ src/main/java/com/javaiii/groupproject/AutoPartsStore/services/consumers/commands/SupplierCommand.java
@@ -1,11 +1,8 @@
 package com.javaiii.groupproject.AutoPartsStore.services.consumers.commands;
 
-import lombok.Data;
-
-@Data
 public class SupplierCommand {
     private String companyName;
-    private String contactName;
+    private String contactPerson;
     private String primaryPhone;
     private String secondaryPhone;
     private String website;
@@ -14,4 +11,87 @@ public class SupplierCommand {
     private String state;
     private String zipCode;
     private String notes;
+
+    public SupplierCommand() {
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getPrimaryPhone() {
+        return primaryPhone;
+    }
+
+    public void setPrimaryPhone(String primaryPhone) {
+        this.primaryPhone = primaryPhone;
+    }
+
+    public String getSecondaryPhone() {
+        return secondaryPhone;
+    }
+
+    public void setSecondaryPhone(String secondaryPhone) {
+        this.secondaryPhone = secondaryPhone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
